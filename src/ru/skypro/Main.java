0f6df8e153928dcfe15d1cf3e1b0ac725d73fcce
Илья <fetisov.ilya.andreevich@gmail.com>
2022-06-05 23:59:48 +0300

    Finished

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,6 +2,21 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName + ".");
 
+//        Task_2
+
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника - " + fullNameUp + ".");
+
+//        Task_3
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameReplace = fullName.replace("ё", "е");
+        System.out.println("ФИО сотрудника - " + fullNameReplace + ".");
     }
 }
