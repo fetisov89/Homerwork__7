a6e8736085f1115319e246a9aa14bfaeb6c35b25
Илья <fetisov.ilya.andreevich@gmail.com>
2022-06-06 00:05:37 +0300

    Finished

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,6 +2,7 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
+
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
